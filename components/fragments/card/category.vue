a9b61cd5e791a68a1be84beed762f848072fd32a
andsr157 <andikasatrio159@gmail.com>
2023-09-15 16:38:08 +0700

    fix:trying fix category svg error

--- components/fragments/card/category.vue
+++ components/fragments/card/category.vue
@@ -8,11 +8,11 @@ const props = defineProps<{ featureData: FeatureType[] }>()
   <div v-for="data in props.featureData" :key="data.title">
     <div class="flex flex-col justify-center items-center lg:gap-5">
       <div class="w-16 h-16 lg:w-[126px] lg:h-[126px]">
-        <img src="~/assets/feature/handphone.svg" alt="" class="w-full" />
+        <img src="" alt="" class="w-full" />
       </div>
       <div class="text-base font-semibold px-5 mt-3 lg:mt-0 text-center w-36">
         {{ data.title }}
       </div>
     </div>
-  </div>
+  </div>  
 </template>
